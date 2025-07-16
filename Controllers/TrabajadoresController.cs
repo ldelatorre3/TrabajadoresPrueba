107201b2132eb3f6923e5399599c7ef2392d0189
Lorenzo De La Torre <lorenzo.de.la.torre@hotmail.com>
2025-07-16 16:14:46 -0500

    Paso 4: Implementar Create con selects dinámicos

--- Controllers/TrabajadoresController.cs
+++ Controllers/TrabajadoresController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TrabajadoresPrueba.Data;
@@ -24,6 +25,49 @@ namespace TrabajadoresPrueba.Controllers
                 .ToListAsync();
 
             return View(trabajadores);
+        }
+
+        // GET: Trabajadores/Create
+        public IActionResult Create()
+        {
+            ViewData["Departamentos"] = new SelectList(_context.Departamentos, "Id", "NombreDepartamento");
+            ViewData["Provincias"] = new SelectList(_context.Provincias, "Id", "NombreProvincia");
+            ViewData["Distritos"] = new SelectList(_context.Distritos, "Id", "NombreDistrito");
+
+            ViewData["TipoDocumento"] = new SelectList(new[]
+            {
+                new { Value = "DNI", Text = "DNI" },
+                new { Value = "RUC", Text = "RUC" },
+                new { Value = "CE",  Text = "Carné de Extranjería" }
+            }, "Value", "Text");
+
+            return View();
+        }
+
+        // POST: Trabajadores/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Trabajador trabajador)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(trabajador);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Recargar combos si falla
+            ViewData["Departamentos"] = new SelectList(_context.Departamentos, "Id", "NombreDepartamento");
+            ViewData["Provincias"] = new SelectList(_context.Provincias, "Id", "NombreProvincia");
+            ViewData["Distritos"] = new SelectList(_context.Distritos, "Id", "NombreDistrito");
+            ViewData["TipoDocumento"] = new SelectList(new[]
+            {
+                new { Value = "DNI", Text = "DNI" },
+                new { Value = "RUC", Text = "RUC" },
+                new { Value = "CE",  Text = "Carné de Extranjería" }
+            }, "Value", "Text");
+
+            return View(trabajador);
         }
     }
 }
--- obj/Debug/net8.0/TrabajadoresPrueba.AssemblyInfo.cs
+++ obj/Debug/net8.0/TrabajadoresPrueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrabajadoresPrueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c366cdb1b0a09cdccb32e63a452378b8c94f509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e96eab2936047d3cc3a95aecd65ac9006dc9663")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrabajadoresPrueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrabajadoresPrueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
